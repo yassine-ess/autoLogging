93894822f8b58e93117b73cb3cdc75a14fe64eec
Yassine Essiyedali <essiysn@gmail.com>
2025-11-30 14:08:47 +0100

    convert to ECMAScript

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,15 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest', // Use ts-jest preset to handle TypeScript files
+export default {
+  preset: 'ts-jest/presets/default-esm', // Use ts-jest ESM preset
   testEnvironment: 'node', // Set the test environment to Node.js
+  extensionsToTreatAsEsm: ['.ts'], // Treat .ts files as ES modules
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Map .js imports to their TypeScript sources
+  },
   transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Use ts-jest to transform TypeScript files
+    '^.+\\.tsx?$': ['ts-jest', {
+      useESM: true, // Enable ESM support in ts-jest
+    }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Recognize TypeScript file extensions
   testMatch: ['**/test/**/*.test.ts'], // Match test files in the test folder
